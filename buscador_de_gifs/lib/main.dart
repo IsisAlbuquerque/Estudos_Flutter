877a99b570662b14860f5261d804e381c64791cf
Isis Albuquerque <isis.alb99@gmail.com>
2020-10-22 15:37:51 -0300

    Projeto buscador_de_gifs finalizado. Adicionado fade in aos gifs ainda em carregamento.

--- buscador_de_gifs/lib/main.dart
+++ buscador_de_gifs/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-
 import 'package:buscador_de_gifs/ui/home_page.dart';
+import 'ui/gif_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+    ),
   ));
 }
